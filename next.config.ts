0cc893fabfec3cea1242b3a9b93ec22b9030ec71
pemmer.ferenc <pemmer.ferenc@gmail.com>
2025-01-14 17:20:11 +0100

    b5

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export", // Statikus exportálás beállítása
-  basePath: "/resume", // Cseréld ki a repository nevére
-  trailingSlash: true, // Helyes routing támogatása
+  output: "export",
+  basePath: "/resume",
+  trailingSlash: true,
 };
 
 export default nextConfig;
